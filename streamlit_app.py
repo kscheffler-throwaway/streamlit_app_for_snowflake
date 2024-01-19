9be28c65e605b3d3dfd1150e8c824865241778c6
kscheffler-throwaway <157031781+kscheffler-throwaway@users.noreply.github.com>
2024-01-19 13:02:19 +0100

    Added dynamic loading from Snowflake.

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,6 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 streamlit.text(fruityvice_response.json())
 
@@ -44,3 +43,11 @@ my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
+
+my_cur.execute("select * from fruit_load_list")
+my_data_rows = my_cur.fetchall()
+streamlit.header("The fruit load list contains:")
+streamlit.dataframe(my_data_rows)
+
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','')
+streamlit.write('Thanks for adding ', add_my_fruit)
